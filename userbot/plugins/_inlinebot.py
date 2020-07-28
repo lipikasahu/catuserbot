e8f19f2c9dae50346aa353a3b999c5e6e1b518c6
Lipika Sahu <68530664+lipikasahu@users.noreply.github.com>
2020-07-28 15:08:12 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -36,7 +36,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Aary bas kar Bhai !! Tab se dabate jaa rha h, Khudka bot bana!"
+            reply_pop_up_alert = "Don't touch it or i will kill you XD!"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
 
